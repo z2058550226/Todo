ad57598c1ad0e67ed2609b4c4209875d94aa6cc2
suikajy <2058550226@qq.com>
2019-10-03 12:49:54 +0800

    add foreground service to remind user

---
+++ app/src/main/java/com/practice/todo/LocationService.kt
@@ -0,0 +1,7 @@
+package com.practice.todo
+
+/**
+ * Created by suikajy on 2019.10.3
+ */
+class LocationService {
+}
--- app/src/main/java/com/practice/todo/util/RemindUtil.kt
+++ app/src/main/java/com/practice/todo/util/RemindUtil.kt
@@ -1,10 +1,12 @@
 package com.practice.todo.util
 
+import android.app.Notification.VISIBILITY_SECRET
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import androidx.core.app.NotificationCompat
 import com.practice.todo.App
 import com.practice.todo.MainActivity
@@ -16,8 +18,8 @@ import com.practice.todo.storage.database.entity.TodoItem
  */
 object NotificationUtil {
 
-    const val NOTIFICATION_CHANNEL_ID = "10001"
-    const val default_notification_channel_id = "default"
+    private const val NOTIFICATION_CHANNEL_ID = "10051"
+    private const val default_notification_channel_id = "todo_practice2"
 
     fun notification(todoItem: TodoItem) {
         val notificationIntent = Intent(App.instance, MainActivity::class.java)
@@ -32,18 +34,29 @@ object NotificationUtil {
                 default_notification_channel_id
             )
         mBuilder.setContentTitle(todoItem.title)
-        mBuilder.setContentIntent(pendingIntent)
-        mBuilder.setSmallIcon(R.drawable.ic_launcher)
-        mBuilder.setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .setContentText("You can mark this todo is done")
+            .setSmallIcon(R.drawable.ic_launcher)
+            .setAutoCancel(true)
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             val importance = NotificationManager.IMPORTANCE_HIGH
-            val notificationChannel = NotificationChannel(
+            val channel = NotificationChannel(
                 NOTIFICATION_CHANNEL_ID,
                 "NOTIFICATION_CHANNEL_NAME",
                 importance
             )
+            channel.enableVibration(true)
+            channel.canBypassDnd()
+            channel.lockscreenVisibility = VISIBILITY_SECRET
+            channel.enableLights(true)
+            channel.lightColor = Color.RED
+            channel.audioAttributes
+            channel.group
+            channel.setBypassDnd(true)
+            channel.shouldShowLights()
+
             mBuilder.setChannelId(NOTIFICATION_CHANNEL_ID)
-            mNotificationManager.createNotificationChannel(notificationChannel)
+            mNotificationManager.createNotificationChannel(channel)
         }
         mNotificationManager.notify(System.currentTimeMillis().toInt(), mBuilder.build())
     }
